de5ff640c196a96a07d0653139206786af9559ee
MurzinR <52663493+MurzinR@users.noreply.github.com>
2019-08-14 10:55:31 +0300

    Исправлена ошибка.

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 config = {'SECRET_KEY': os.environ.get('SECRET_KEY') or 'you-will-never-guess',
-          'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db')),
+          'SQLALCHEMY_DATABASE_URI': os.environ.get('DATABASE_URL'),
           'SQLALCHEMY_TRACK_MODIFICATIONS': False,
           'REDIS_URL': os.environ.get('REDIS_URL') or 'redis://'}
